0238b8d34e77e6183167d78efa5f4741c4d86a3f
jon-chow <jon.chowyyc@gmail.com>
2024-02-28 21:46:22 -0800

    feat: update cypress scripts

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -2,14 +2,25 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   component: {
-    devServer:{
+    devServer: {
       framework: 'react',
-      bundler: 'vite',
+      bundler: 'vite'
     }
   },
   e2e: {
     baseUrl: 'http://localhost:4321',
     supportFile: false,
     screenshotOnRunFailure: false
-  }
+  },
+  browsers: [
+    {
+      name: 'electron',
+      channel: 'stable',
+      family: 'chromium',
+      displayName: 'Electron',
+      version: '114.0.5735.289',
+      path: '',
+      majorVersion: 114
+    }
+  ]
 });
--- client/cypress/e2e/index.cy.ts
+++ client/cypress/e2e/index.cy.ts
@@ -2,6 +2,7 @@
 
 it('titles are correct', () => {
   const page = cy.visit('/');
+  cy.viewport('macbook-15');
 
   page.get('title').should('have.text', 'Prometheus — Fire Music!');
 });
